4e43a2212d6ea945145d7112328447dbd1e3e3de
Bruno Almeida de Matos <65208006+BrunoAlm@users.noreply.github.com>
2022-07-21 22:54:25 -0300

    upchanfres

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,10 @@
-const btnMobile = document.getElementById('btn-mobile');
+const btnMenu = document.getElementById('btn-menu');
+const btnVoltaMenu = document.getElementById('volta-menu');
 
-function toggleMenu(){
+function toggleMenu() {
     const nav = document.getElementById('nav');
     nav.classList.toggle('active');
 }
 
-btnMobile.addEventListener('click', toggleMenu);
+btnMenu.addEventListener('click', toggleMenu);
+btnVoltaMenu.addEventListener('click', toggleMenu);
