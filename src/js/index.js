9c5a5ab0ca6dd5b4091fff04544142a9c082bd38
Bruno Almeida de Matos <65208006+BrunoAlm@users.noreply.github.com>
2022-07-25 20:32:33 +0000

    comentado

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,12 @@
 const btnMenu = document.getElementById('abre-menu');
 const btnVoltaMenu = document.getElementById('volta-menu');
 
-function toggleMenu() {
+function abreMenu() {
     const drawer = document.getElementById('nav-do-lado');
     drawer.classList.toggle('expandiu');
     btnMenu.classList.toggle('expandiu');
     btnVoltaMenu.classList.toggle('expandiu');
 }
 
-btnMenu.addEventListener('click', toggleMenu);
-btnVoltaMenu.addEventListener('click', toggleMenu);
+btnMenu.addEventListener('click', abreMenu);
+btnVoltaMenu.addEventListener('click', abreMenu);
