b91e50c1df168a88abeea2fb88fe8a4c8f2637f5
Bruno Almeida de Matos <65208006+BrunoAlm@users.noreply.github.com>
2022-07-27 18:56:33 +0000

    hum

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
 const btnMenu = document.getElementById('abre-menu');
 const btnVoltaMenu = document.getElementById('volta-menu');
+const drawer = document.getElementById('nav-do-lado');
 
 function abreMenu() {
-    const drawer = document.getElementById('nav-do-lado');
     drawer.classList.toggle('expandiu');
     btnMenu.classList.toggle('expandiu');
     btnVoltaMenu.classList.toggle('expandiu');
